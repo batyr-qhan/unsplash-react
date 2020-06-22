af6288d9dd1e4bc60b81682bdb89f7b27e1cc58b
cruzador <batyrqh@protonmail.com>
2020-06-22 13:46:22 +0600

    run build

---
+++ server/build/precache-manifest.8533a278f4040bf7fd415463bfde12d5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d310ca4bad3ae5b59a5084f368e2c16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17867d406835c58f32df",
+    "url": "/static/css/main.99ff525c.chunk.css"
+  },
+  {
+    "revision": "35c8321310c0e27d6ad0",
+    "url": "/static/js/2.007e39ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.007e39ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17867d406835c58f32df",
+    "url": "/static/js/main.011a8cea.chunk.js"
+  },
+  {
+    "revision": "7dd049e788f716dcd20a",
+    "url": "/static/js/runtime-main.fb3b939b.js"
+  },
+  {
+    "revision": "7067970e10898f1daf8ad4b5a462663f",
+    "url": "/static/media/download_24px_rounded.7067970e.svg"
+  },
+  {
+    "revision": "c6ac71a12faffd90b7e2abd405067f6e",
+    "url": "/static/media/favorite_24px_rounded.c6ac71a1.svg"
+  },
+  {
+    "revision": "489af8abb42619702e184b3f7a5ae148",
+    "url": "/static/media/maximize-2.489af8ab.svg"
+  }
+]);
